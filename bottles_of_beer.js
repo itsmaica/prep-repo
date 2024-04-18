b3add02154edd0f1e970c8d248ff50c9ef6a2269
Maica <maicasantos90@gmail.com>
2024-04-18 00:22:54 -0700

    beer

--- bottles_of_beer.js
+++ bottles_of_beer.js
@@ -43,20 +43,59 @@ Helper functions are a totally cool idea...
 2. For each num, print out the song lines at a 1 second delay
 3. After each round there should be new line
 4. repeat that pattern until we're out of beer :(
-5. 
+5. When there is no more beer, print No More Bottles of beer on the wall...
+
+1. Identify the base case: num === 0;
+    a. console.log("No more bottles of beer on the wall... 😢")
+    b. return / stop recursing
+2. The Recursive Step: decrement num || num -= 1
+    a. console.log("num bottles of beers n the wall")
+    b. 1 second need to pass between each line
+    c. an empty line after each round.
+3. Recursive call
+    a. setTimeout(bottlesOfBeerOnTheWll, delay, num)
 ***********************************************************************/
 
-
-const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
+const readline = require("node:readline");
+const { stdin: input, stdout: output } = require("node:process");
+const { type } = require("node:os");
 
 const rl = readline.createInterface({ input, output });
 
 // console.log(rl)
 
 // instead of the default cb, we're going to add one of our own making.
-rl.question('How many bottles of beers on the wall? ', bottlesOfBeerOnTheWall());
+rl.question("How many bottles of beers on the wall? ", bottlesOfBeerOnTheWall);
 
 function bottlesOfBeerOnTheWall(num) {
+  // console.log("num:", num)
+  // console.log(`this many: ${num}`)
 
+  if (typeof num === "string") num = +num;
+  // console.log(typeof num)
+
+  //base case
+  if (num === 0) {
+    console.log(`No more bottles of beer on the wall... 🥲`);
+    rl.close();
+    return;
+  }
+
+  //opening statement
+  console.log(`${num} bottles of beer on the wall...🍻`);
+
+  //follow up
+  setTimeout(console.log, 1000, `${num} bottles of beer...🍻`);
+
+  //create an empty line
+  setTimeout(console.log, 1000, ``);
+
+  //start new round
+  setTimeout(console.log, 1000, `Take one down and pass it around...🍻`)
+
+  //decrement the num
+  setTimeout(console.log, 3000, `${num -= 1} bottles of beer on the wall...🍻`)
+
+  //recursive call
+  setTimeout(bottlesOfBeerOnTheWall, 5000, num)
 }

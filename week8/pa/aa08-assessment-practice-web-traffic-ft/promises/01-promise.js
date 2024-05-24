14c0dddd9630a8bfe325fdc6a8a736b47ae8ba6d
Maica <maicasantos90@gmail.com>
2024-05-24 00:13:10 -0700

    no more .git

--- week8/pa/aa08-assessment-practice-web-traffic-ft/promises/01-promise.js
+++ week8/pa/aa08-assessment-practice-web-traffic-ft/promises/01-promise.js
@@ -10,7 +10,42 @@ const growPlant = makePromise('growPlant');
 
  ******************************************************************************/
 
-// Your code here 
+// Your code here
+
+// ! GOTTA RUN IN MOCHA TO SEE THE LOGS!
+// console.log('soil -> \n',addSoil) // shows a func
+// console.log('what dis? ', addSoil()) // Promise { true } -> a promise!!
+
+// cannot be logged outside of the main func?
+async function potPlant1(seed) {
+  // console.log(addSoil)
+
+  // addSoil()
+  await addSoil() // * resolve it -> it returns a boolean
+  console.log(await addSoil()) // this also works
+
+  // what is plant seed?
+  console.log('🌱',plantSeed(seed)) // Plant { seed: 'Basil', fullyGrown: false }
+
+  // * Need to resolve it
+  const plant = await plantSeed(seed)
+
+  return growPlant(plant)
+
+}
+
+const potPlant = seed => {
+  // ! MUST RETURN TO PASS THE LAST SPEC
+  return addSoil()
+    .then(() => plantSeed(seed))
+    .then(plant => growPlant(plant))
+}
+
+// console.log(plantSeed)
+// console.log(growPlant)
+
+
+// potPlant()
 
 
 /*******************************************************************************
--- week8/pa/aa08-assessment-practice-web-traffic-ft/promises/02-fetch.js
+++ week8/pa/aa08-assessment-practice-web-traffic-ft/promises/02-fetch.js
@@ -1,5 +1,5 @@
 if (!fetch) {
-  var fetch = require('./test/node-fetch')(1);
+  var fetch = require("./test/node-fetch")(1);
 }
 
 /**
@@ -8,4 +8,51 @@ if (!fetch) {
 
  ******************************************************************************/
 
-// Your code here 
+// Your code here
+// * Two ways to go about this, can write a straight up fetch, or put it inside of a function
+
+
+const options = {
+  method: "PUT",
+  headers: {
+    "Content-Type":"application/json"
+  },
+  body: JSON.stringify({
+    "color": "green"
+  })
+}
+
+fetch("/colors/1", options).then(res => {
+
+  console.log('res',res) // ! whole ass response body obj. we can't work this yet. this is lik raw materials, we need to refine it with .json()
+
+  // * if we multi line, gotta explicitly return this bitch
+
+  // ! returns a promise Promise { { id: 1, color: 'green' } }
+  // * .json is particular to the response object. This returns a promise read all about it.
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/Response/json
+  return res.json()
+
+}).then(resBody => console.log(resBody))
+
+// ! write this in a function and utilize async
+const goFetch2 = async () => {
+  // await this fetch, and save its result
+  const response = await fetch("/colors/1", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      color: "green",
+    }),
+  });
+
+  // console.log(res); // ! returns the response object, so we need to .json our raw materials
+  const data = await response.json(); // ! don't put semi colon???
+  console.log(data);
+};
+
+// ! To pass specs you must invoke the function
+goFetch2()

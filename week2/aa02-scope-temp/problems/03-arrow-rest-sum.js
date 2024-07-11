dce82da6ece11f160f2f176229da78ec48432e2e
Maica <maicasantos90@gmail.com>
2024-07-10 23:52:17 -0700

    scope 3

--- week2/aa02-scope-temp/problems/03-arrow-rest-sum.js
+++ week2/aa02-scope-temp/problems/03-arrow-rest-sum.js
@@ -6,12 +6,28 @@ arrow function syntax.
 **Hint**: Use rest parameter syntax!
 
 Examples:
-arrowRestSum(3,5,6); // => 14
-arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
-arrowRestSum(0); // => 0
 ***********************************************************************/
 
-// Your code here 
+// Your code here
+// ! one liner using reduce to get sum
+const arrowRestSum1 = (...args) => args.reduce((acc, curr) => acc + curr, 0)
+
+const arrowRestSum = (...args) => {
+  // create a res variable;
+  let res = 0;
+  //
+  // args.forEach(num => res += num)
+
+  for (let num of args) {
+    res += num;
+  }
+
+  return res
+}
+
+console.log(arrowRestSum(3,5,6)); // => 14
+console.log(arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 45
+console.log(arrowRestSum(0)); // => 0
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
